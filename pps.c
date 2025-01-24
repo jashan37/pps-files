ec6d4f801890306253e45d448bafc66a43debbb1
jashan37 <jashanpreetsinghrv3@gmail.com>
2025-01-24 16:12:51 +0530

    Add files via upload

---
+++ pps.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+
+{
+
+  return 0;
+}
+
+// first lab--------------------------------------------------------------------------------------------------------->
+
+// clrscr();
+
+// printf("hello word");
+// printf("my name is jashanpreetsingh \n");
+// printf("my roll number : 241003 \n");
+// printf("i am the student of b.tech \n");
+
+//  getch();
+
+// second lad-------------------------------------------------------------------------------------------------------->
+
+/*  int a = 20;
+  char b = 'B';
+  float c = 5.8;
+
+  printf("the value of int is :%d \n", a);
+  printf("the value of char is :%c \n", b);
+  printf("the value of float is :%f \n", c);
+*/
+
+// my practice codes-------------------------------------------------------------------------------------------------->
+
+// int number;
+//  printf(" enter a number:");
+//  scanf("%d",&number);
+//  printf("cube of the number:%d",number*number*number);
+//------------------------------------------------------------------------------------------------------------------------>
+
+// int x, y, z;
+//
+// printf("enter number: ");
+// scanf("%d", &x);
+// printf("enter one more number :");
+// scanf("%d", &y);
+// z = x + y;
+// printf("sum of the two number:%d", z);
+
+//----------------------------------------------------------------------------------------------------------------------->
+
+// int age = 25;
+
+// float salary = 2500.5;
+// char initial = 'J';
+//// Later in the program, you can change the value of the variable
+// age = 30;
+// salary = 3000.0;
+//
+// printf("age:%d",age);
+
+//----------------------------------------------------------------------------------------------------------------------->
+
+// int number = 0;
+//
+// printf("enter number to find its even or not : ");
+// scanf("%d", &number);
+// if (number % 2 == 0)
+// {
+//   printf("%d is the even number", number);
+// }
+
+//------------------------------------------------------------------------------------------------------------------------>
+
+// int a, b, c;
+// printf("enter number to find which one is largest :");
+// scanf("%d %d %d", &a, &b, &c);
+// if (a > b && a > c)
+//{
+//   printf("%d is largest", a);
+// }
+// if (b > a && b > c)
+//{
+//   printf("%d is largest", b);
+// }
+// if (c > a && c > b)
+//{
+//   printf("%d is largest", c);
+// }
+// if (a==b && a==c && b==c)
+//{
+//   printf("all are equal");
+// }
+// if (a==b)
+//{
+//   printf("%d %d is equal", a, b);
+// }
+// if (b==c)
+//{
+//   printf("%d %d is equal", b, c);
+// }
+
+//-------------------------------------------------------------------------------------------------------------------------->
