48cf195c076a03df11c2fcb7ab135f0cc6a52fa3
jashan37 <jashanpreetsinghrv3@gmail.com>
2025-01-24 17:56:27 +0530

    Update pps.c

--- pps.c
+++ pps.c
@@ -4,7 +4,6 @@
 int main()
 
 {
-
   return 0;
 }
 
@@ -101,3 +100,29 @@ int main()
 // }
 
 //-------------------------------------------------------------------------------------------------------------------------->
+
+// int marks;
+//   printf("Enter your marks?");
+//   scanf("%d",&marks);
+//   if(marks > 85 && marks <= 100)
+//   {
+//       printf("Congrats ! you scored grade A ...");
+//   }
+//   else if (marks > 60 && marks <= 85)
+//   {
+//       printf("You scored grade B + ...");
+//   }
+//   else if (marks > 40 && marks <= 60)
+//   {
+//       printf("You scored grade B ...");
+//   }
+//   else if (marks > 30 && marks <= 40)
+//   {
+//       printf("You scored grade C ...");
+//   }
+//   else
+//   {
+//       printf("Sorry you are fail ...");
+//   }
+
+//----------------------------------------------------------------------------------------------------------------------------->
