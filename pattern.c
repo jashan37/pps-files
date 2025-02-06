3af6ace5a8e7ff7ffa44b6c59817872cda2fba67
jashan37 <jashanpreetsinghrv3@gmail.com>
2025-02-06 21:04:28 +0530

    Add files via upload

---
+++ pattern.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+int main()
+{
+ int r,c;
+printf("enter a rows :");
+scanf("%d",&r);
+for (int i = 1; i <=r ; i++){
+    for (int j = 1 ; j <= i; j++)
+
+    {
+    printf("* ");
+        
+    }
+    printf("\n");
+    
+}
+    
+
+
+    return 0;
+}
+// int r,c;
+// printf("enter a rows :");
+// scanf("%d",&r);
+// printf("enter a columns :");
+// scanf("%d",&c);
+// for (int i = 1; i <=r ; i++){
+//     for (int i = 1 ; i <= c; i++)
+//     {
+//     printf("%d",i);
+        
+//     }
+//     printf("\n");
+    
+// }
+//  int r,c;
+// printf("enter a rows :");
+// scanf("%d",&r);
+// for (int i = 1; i <=r ; i++){
+//     for (int j = 1 ; j <= i; j++)
+
+//     {
+//     printf("* ");
+        
+//     }
+//     printf("\n");
+    
+// }
---
+++ q.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+
+
+
+
+    
+
+        return 0;
+}
+
+// ==============================================Write a Program to find the size of int, float, double, and char.====================================>
+
+// int a;
+// char c;
+// float b;
+// double d;
+
+// printf("size of int : %u bytes \n", sizeof(a),a);
+// printf("size of float : %u bytes \n", sizeof(b), b);
+// printf("size of char : %u bytes \n", sizeof(c),c);
+// printf("size of double : %u bytes \n", sizeof(d),d);
+
+// =================================================================Write a Program to Swap the values of two variables.==========================================>
+
+// int a,b,c;
+// printf("enter the value of a :");
+// scanf("%d",&a);
+// printf("enter the value of b :");
+// scanf("%d",&b);
+
+// c=a;
+// a=b;
+// b = c;
+
+// printf("a = %d , b = %d", a,b);
+
+// ================================================================Write a Program to calculate Compound Interest.==============================================>
+
+// double p, t,r, h= 100, CI;
+
+// printf("enter principal (amount) :  ");
+// scanf("%lf",&p);
+// printf("enter time :  ");
+// scanf("%lf",&t);
+// printf("fnter rate :  ");
+// scanf("%lf",&r);
+
+// CI = p*((pow((1+r/ h ),t))) -p;
+
+// printf(" CI is : %lf",CI );
